4024017dfd6fb328e7231b867f22c25bccfa328c
Cyril Moron <cyril.moron@gmail.com>
2020-06-01 12:01:23 +0200

    Some script update after shellcheck

--- .local/bin/add_screen.sh
+++ .local/bin/add_screen.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
+# Reminder of xrandr syntax to add new 2k screen right of current laptop one.
+
+set -o errexit
+set -o nounset
+
 xrandr --output HDMI-2 --mode 2560x1440 --right-of eDP-1
